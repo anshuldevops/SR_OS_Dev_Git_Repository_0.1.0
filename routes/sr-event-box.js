7c57f47e8149880f2a4e985b1db877fe11ed6dc7
rishimehta365 <rishimehta365@gmail.com>
2017-04-16 18:56:03 +0530

    Initial Commit

---
+++ routes/sr-event-box.js
@@ -0,0 +1,16 @@
+var express= require('express');
+var router= express.Router();
+var mongojs= require('mongojs');
+var db= mongojs('mongodb://srankings:srankings123@ds145750.mlab.com:45750/school-rankings',['quotes']);
+
+//GET All School Information
+router.get('/tasks', (req, res, next)=>{
+  db.quotes.find().skip(2).limit(10,(err, quotes)=>{
+    if(err){
+      res.send(err);
+    }
+    res.json(quotes);
+  });
+});
+
+module.exports= router;
