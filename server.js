134feb4fe88534bb275cd754df738ea2dc712fc6
rishimehta365 <rishimehta365@gmail.com>
2017-06-12 15:37:10 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.use('/contact', contact);
 app.use('**', pagenotfound);
 
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
